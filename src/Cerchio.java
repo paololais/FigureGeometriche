1d263a7dca0be5cf9bf6105bb515784b3e598a96
paololais <paolo.lais1@gmail.com>
2022-11-09 12:38:13 +0100

    added equals and hashcode

--- src/Cerchio.java
+++ src/Cerchio.java
@@ -32,4 +32,29 @@ public class Cerchio extends Forma {
     private double extracted() {
         return 2*raggio*Math.PI;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(raggio);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cerchio other = (Cerchio) obj;
+        if (Double.doubleToLongBits(raggio) != Double.doubleToLongBits(other.raggio))
+            return false;
+        return true;
+    }
+    
 }
